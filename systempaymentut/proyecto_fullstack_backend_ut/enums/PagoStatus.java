85326f23ba1aa3ba956ab7babe79326da4180ef0
SMforthewin <smmontoyava@ut.edu.co>
2025-05-01 21:19:41 -0500

    Delete systempaymentut/proyecto_fullstack_backend_ut directory

--- systempaymentut/proyecto_fullstack_backend_ut/enums/PagoStatus.java
+++
@@ -1,7 +0,0 @@
-package com.systempaymentut.proyecto_fullstack_backend_ut.enums;
-
-public enum PagoStatus  {
-    
-    CREADO, VALIDADO, RECHAZADO;
-
-}
--- systempaymentut/proyecto_fullstack_backend_ut/enums/TypePago.java
+++
@@ -1,7 +0,0 @@
-package com.systempaymentut.proyecto_fullstack_backend_ut.enums;
-
-public enum TypePago {
-    
-    EFECTIVO, CHEQUE, TRANSFERENCIA, DEPOSITO;
-
-}
